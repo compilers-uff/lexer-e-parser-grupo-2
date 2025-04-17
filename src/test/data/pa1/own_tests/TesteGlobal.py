be2af79a85a9e036bf7410297f55532313b6659f
ThiagoAnorian <thiagoserra@id.uff.br>
2025-04-16 21:43:53 -0300

    update:testPrototype

--- src/test/data/pa1/own_tests/TesteGlobal.py
+++ src/test/data/pa1/own_tests/TesteGlobal.py
@@ -1,26 +1,30 @@
+lista: [int] = None
+matriz: [[int]] = None
+
+class _Ameba89(_Ameba88):
+    pass
+
 class A(object):
     x: int = 13
-    def get(self: A) -> int:
-        return self.x
 
-lista: [int] = None
-lista = [1, 2, 3, 13, 20]
+    def get10(self: "A") -> int:
+        return 10
 
-buscado = A().x
+    def func1(num1: int, num2: int) -> int:
+        global numero1
+        numero2: int = 11
 
-def func1(num1: int, num2: int) -> int:
-    global numero1
-    numero2: int = 11
+        def func2():
+            nonlocal numero2
 
-    def func2():
-        nonlocal numero2
+            numero3: int = 10
 
-        numero3: int = 10
+            return 9999 * 67
 
-        return numero3
+        return func2()
 
-    return func2()
-
+    def func():
+        pass
 
 def buscaBinaria(lista:[int], buscado:int):
     i = len(lista)//2
@@ -35,11 +39,22 @@ def buscaBinaria(lista:[int], buscado:int):
 
 
 
-i = buscaBinaria(lista, buscado)
-
-if (i==0 or i==15):
+lista = [1, 2, 3, 13, 20]
+i = buscaBinaria(lista, 8989)
+if i>=15:
     pass
+elif not i and 1:
+    for i in lista:
+        print("alo")
 else:
     print("O cara esta na posicao ", i)
     
 i = i*5+10
+
+if True:
+    a = -9
+
+print(lista[i*78 %2] is None)
+lista = []
+lista = []
+x = y = 90
